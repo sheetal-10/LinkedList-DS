cb9e9639e61263da27a077cf548379b616434dae
sheetal-10 <sheetalp257@gmail.com>
2021-08-20 19:34:08 +0530

    Added: welcome message

---
+++ src/com/ds/linkedlist/Main.java
@@ -0,0 +1,20 @@
+package com.ds.linkedlist;
+
+/**
+ * Main function for calling of Linked List Class
+ * To pass data to Linked list to do different operations
+ * @author sheetal
+ * @since 2021-08-20
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to linked lis operation program");
+		/*
+		 * System.out.println("Program for the Simple Linked List");
+		 * 
+		 * LinkedList LinkedList = new LinkedList(); //Object //inserting Elements
+		 * LinkedList.insert(56); LinkedList.insert(30); LinkedList.insert(70);
+		 */
+	}
+
+}
